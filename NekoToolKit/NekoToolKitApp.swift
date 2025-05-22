0dc64fd5963821cfef1979d5a7bbdc6aa6918e64
MakiforDevelop <makifordevelop@gmail.com>
2025-05-22 11:20:45 +0800

    Initial Commit

---
+++ NekoToolKit/NekoToolKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekoToolKitApp.swift
+//  NekoToolKit
+//
+//  Created by 千葉牧人 on 2025/5/22.
+//
+
+import SwiftUI
+
+@main
+struct NekoToolKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
